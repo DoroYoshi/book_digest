16c9feef5408ab89659a04585fe290b637adc05f
DoroYoshi <yoshimatsu44dy@gmail.com>
2021-08-03 21:21:18 +0900

    showアクション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,10 @@ class BooksController < ApplicationController
     redirect_to @book, notice: "本を登録完了"
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   private
 
   def book_params
