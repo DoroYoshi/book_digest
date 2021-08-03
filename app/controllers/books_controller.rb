93f0fac7a3618afc9694c542322caf80b38afb90
DoroYoshi <yoshimatsu44dy@gmail.com>
2021-08-04 06:43:26 +0900

    モジュールに処理を切り出しと、edit,updateアクションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "更新完了"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def book_params
